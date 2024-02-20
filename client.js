923d57ef68fc97407b48d238a2e9b605f8c64719
hoycmd <145314654+hoycmd@users.noreply.github.com>
2024-02-21 01:45:45 +0700

    Update client.js

--- client.js
+++ client.js
@@ -64,37 +64,8 @@ roomInventory.BuildInfinity.Value = true;
 
 // моментальный спавн
 Spawns.GetContext().RespawnTime.Value = 0;
-
-
  player.contextedProperties.SkinType.Value = 2;
- player.inventory.Main.Value = true;  
- player.inventory.MainInfinity.Value = true;  
- player.inventory.Secondary.Value = true;   
- player.inventory.SecondaryInfinity.Value = true;  
- player.inventory.Melee.Value = true;  
- player.inventory.Explosive.Value = true;  
- player.inventory.ExplosiveInfinity.Value = true;  
- player.inventory.Build.Value = true;  
- player.inventory.BuildInfinity.Value = true;  
- player.Build.FlyEnable.Value = true;  
-
-player.Build.FloodFill.Value = true;      
-player.Build.FillQuad.Value = true;      
-player.Build.RemoveQuad.Value = true;      
-player.Build.BalkLenChange.Value = true;      
-player.Build.FlyEnable.Value = true;      
-player.Build.SetSkyEnable.Value = true;    
-    
-player.Build.GenMapEnable.Value = true;    
-player.Build.ChangeCameraPointsEnable.Value = true;      
-player.Build.QuadChangeEnable.Value = true;      
-player.Build.BuildModeEnable.Value = true;      
-player.Build.CollapseChangeEnable.Value = true;      
-player.Build.RenameMapEnable.Value = true;      
-player.Build.ChangeMapAuthorsEnable.Value = true;      
-player.Build.LoadMapEnable.Value = true;      
-player.Build.ChangeSpawnsEnable.Value = true;      
-player.Build.BuildRangeEnable.Value = true;
+ 
 	
 
 
